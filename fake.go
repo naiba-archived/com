2fec78d5abba6893944c00b07b9cb43c3bfb2f16
Naiba <1@5.nu>
2018-09-12 11:20:12 +0800

    remove less used snippet

--- fake.go
+++ fake.go
@@ -30,17 +30,16 @@ func RandomIP() string {
 	return strconv.Itoa(rand.Intn(254)+1) + "." + strconv.Itoa(rand.Intn(254)+1) + "." + strconv.Itoa(rand.Intn(254)+1) + "." + strconv.Itoa(rand.Intn(254)+1)
 }
 
-const letterBytes = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-const (
-	letterIdxBits = 6                    // 6 bits to represent a letter index
-	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
-	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
-)
-
-var src = rand.NewSource(time.Now().UnixNano())
-
 //RandomString get a random string
 func RandomString(n int) string {
+	const letterBytes = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	const (
+		letterIdxBits = 6                    // 6 bits to represent a letter index
+		letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+		letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
+	)
+
+	var src = rand.NewSource(time.Now().UnixNano())
 	b := make([]byte, n)
 	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
 	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
